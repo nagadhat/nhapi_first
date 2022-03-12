473e22ea5baac5e6dbe152ca5f427ac14da46d1a
biplob192 <biplob.net2@gmail.com>
2022-03-12 14:52:35 +0600

    Create Migration

---
+++ database/migrations/2022_03_10_101133_create_products_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_categories', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->integer('product_id');
+            $table->integer('category_id');
+            $table->integer('author_id')->nullable();
+            $table->integer('last_modified_id')->nullable();
+            $table->dateTime('last_modified')->nullable();
+            $table->string('last_modified_ip')->nullable();
+            $table->string('last_modified_device')->nullable();
+            $table->string('category_name')->nullable();
+            $table->timestamps()->default('current_timestamp()');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_categories');
+    }
+}
