caf4af9bfbfecc40a9bf89f22bbbf5dffe7ac198
biplob192 <biplob.net2@gmail.com>
2022-03-26 15:49:23 +0600

    New Migration

--- database/migrations/2022_03_10_101109_create_brands_table.php
+++ database/migrations/2022_03_10_101109_create_brands_table.php
@@ -22,6 +22,7 @@ class CreateBrandsTable extends Migration
             $table->string('logo', 300)->nullable();
             $table->string('banner', 300)->nullable();
             $table->timestamps();
+            // $table->timestamps();
         });
     }
 
