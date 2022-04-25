625dabe0f3030343179aa6bc171833c11b3ce1f3
biplob192 <biplob.net2@gmail.com>
2022-04-25 22:17:16 +0600

    Minor

--- app/Http/Controllers/API/RequisitionIssueController.php
+++ app/Http/Controllers/API/RequisitionIssueController.php
@@ -8,14 +8,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Outlet;
 use App\Models\OutletRequisition;
-use Illuminate\Validation\Validator;
+// use Illuminate\Validation\Validator;
+use Validator;
 // use Illuminate\Http\Response;
 
 class RequisitionIssueController extends BaseController
 {
     protected $requisitionIssueRepository;
     protected $outlet;
-    public function __construct(RequisitionIssueRepository $requisitionIssueRepository, Outlet $outlet) 
+    public function __construct(RequisitionIssueRepository $requisitionIssueRepository, Outlet $outlet)
     {
         $this->requisitionIssueRepository = $requisitionIssueRepository;
         $this->outlet = $outlet;
@@ -30,13 +31,13 @@ class RequisitionIssueController extends BaseController
         if(empty($this->outlet::find($outletID))){
             return $this->sendError('Invalid Outlet ID', ['error'=>'Outlet Not Found!']);
         }
-        
+
         foreach($requisitionProduct as $requisition){
             $validator = Validator::make($requisition, [
                 'product_id' => 'required|integer',
                 'product_quantity' => 'required|integer',
             ]);
-       
+
             if($validator->fails()){
                 return $this->sendError('Validation Error.', $validator->errors());
             }
@@ -48,9 +49,9 @@ class RequisitionIssueController extends BaseController
             'data' => $this->requisitionIssueRepository->newRequisition($request),
         ]);
     }
-    
+
     public function outletIssues($outletID)
-    {   
+    {
         return response()->json([
             'data' => $this->requisitionIssueRepository->outletIssues($outletID),
         ]);
