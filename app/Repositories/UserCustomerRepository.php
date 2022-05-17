ea458972db8fa28f606974e4facd8b357a47e555
biplob192 <biplob.net2@gmail.com>
2022-05-17 13:57:59 +0600

    update userDetails by userName

--- app/Repositories/UserCustomerRepository.php
+++ app/Repositories/UserCustomerRepository.php
@@ -5,20 +5,44 @@ namespace App\Repositories;
 use App\Interfaces\UserCustomerRepositoryInterface;
 use App\Models\User;
 use App\Models\Address_assign;
+use App\Models\Address;
 
 class UserCustomerRepository implements UserCustomerRepositoryInterface 
 {
     protected $user;
     protected $address_assign;
-    public function __construct(User $user, Address_assign $address_assign){
+    protected $address;
+    public function __construct(User $user, Address_assign $address_assign, Address $address){
         $this->user = $user;
         $this->address_assign = $address_assign;
+        $this->address = $address;
     }
 
     public function userDetailsByUserName($userName) 
     {
-        return $this->user::leftjoin('address_assign', 'users.username', '=', 'address_assign.username')
-        ->leftjoin('address', 'address_assign.address_id', '=', 'address.id')
-        ->where('users.username', $userName)->select('users.*', 'address.address_1', 'address.address_2')->first();
+        $user = $this->user::
+        leftjoin('user_customers', 'users.username', '=', 'user_customers.username')
+        ->where('users.username', $userName)
+        ->select('users.*', 'user_customers.first_name')
+        ->get();
+
+        for($i = 0; $i < count($user); $i++) {
+            $addresses = $this->address_assign::
+            leftjoin('address', 'address_assign.address_id', '=', 'address.id')
+            ->where('username', $userName)->get();
+
+            $items = array();
+            foreach ($addresses as $address) {
+                $items[] = array(
+                    'address_1' => $address->address_1,
+                    'area' => $address->area,
+                    'postal_code' => $address->postal_code,
+                    'city' => $address->city,
+                    'country' => $address->country,
+                );
+            }
+            $user[$i]['addresses'] = $items;
+        }
+        return $user;
     }
 }
